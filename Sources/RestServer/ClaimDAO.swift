01e0343d95044c0893185018b0bf0e58d039c248
anneposo <anneposo@anneposos-MacBook-Pro.local>
2020-10-24 02:13:13 -0700

    Implemented getAll()

--- Sources/RestServer/ClaimDAO.swift
+++ Sources/RestServer/ClaimDAO.swift
@@ -6,6 +6,7 @@
 //
 
 import SQLite3
+import Cocoa
 
 struct Claim : Codable {
     // define properties then initialize values with initializer (init)
@@ -37,5 +38,31 @@ class ClaimDAO {
             print("Failed to insert a Claim record due to error \(errcode)")
         }
         sqlite3_close(conn) // close db connection
+    }
+    
+    func getAll() -> [Claim] { // returns list of Person objects
+        var cList = [Claim]()
+        var resultSet : OpaquePointer? // record returned from sqlStr query
+        // sql query we will run, passed as an arg to sqlite3_prepare_v2()
+        let sqlStr = "select id, title, date, isSolved from claim"
+        let conn = Database.getInstance().getDBConnection()
+        
+        // 4th parameter is
+        if sqlite3_prepare_v2(conn, sqlStr, -1, &resultSet, nil) == SQLITE_OK {
+            // step through each record inside resultSet and exit if no more rows
+            while(sqlite3_step(resultSet) == SQLITE_ROW) {
+                let id_val = sqlite3_column_text(resultSet, 0)
+                let id = String(cString: id_val!)
+                let title_val = sqlite3_column_text(resultSet, 1)
+                let title = String(cString: title_val!)
+                let date_val = sqlite3_column_text(resultSet, 2)
+                let date = String(cString: date_val!)
+                let solve_val = sqlite3_column_int(resultSet, 3)
+                let isSolved = Bool(solve_val as NSNumber)
+
+                cList.append(Claim(i: id, t: title, d: date, solve: isSolved))
+            }
+        }
+        return cList
     }
 }
