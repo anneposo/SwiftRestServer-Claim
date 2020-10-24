a4c8466d3ec9ab152f43d1c44260e5179861c849
anneposo <anneposo@anneposos-MacBook-Pro.local>
2020-10-24 00:38:39 -0700

    Implemented /ClaimService/add post method

--- Sources/RestServer/main.swift
+++ Sources/RestServer/main.swift
@@ -2,7 +2,22 @@ import Kitura
 
 let router = Router()
 
-let dbObj = Database.getInstance()
+//let dbObj = Database.getInstance()
+
+router.post("/ClaimService/add") {
+    request, response, next in
+    let body = request.body // gets string from http message body
+    let jObj = body?.asJSON // convert json string to json object
+    
+    if let jDict = jObj as? [String:String] { // if jDict is not nil
+        if let i = jDict["id"], let t = jDict["title"], let d = jDict["date"], let solve = Int(jDict["isSolved"]!) {
+            let cObj = Claim(i: i, t: t, d: d, solve: solve)
+            ClaimDAO().addPerson(cObj: cObj)
+        }
+    }
+    response.send("The Person record was successfully inserted (via POST method).")
+    next()
+}
 
 // port number your server will listen to, using object "router"
 Kitura.addHTTPServer(onPort: 8020, with: router)
